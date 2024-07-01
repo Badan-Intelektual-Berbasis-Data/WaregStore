b739669bd4a0d302daa3e9a3037c89e990660ad7
uqizeroxl <mohkholqimaulana@gmail.com>
2024-07-01 22:40:45 +0700

    mengubah style

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,14 +3,23 @@ import React from 'react';
 
 const Sidebar = ({opened}) => {
   return (
-    <div className={`h-screen bg-gray-500 py-8 text-center overflow-hidden duration-150 ease-in ${!opened ? 'w-0' : 'w-[250px]'}`}>
+    <div className={`max-h-screen bg-[#808080] py-8 text-center overflow-hidden duration-150 ease-in ${!opened ? 'w-0' : 'w-[250px]'}`}>
       <h1 className='font-bold text-2xl text-white'>WaregStore</h1>
       <div className='flex flex-col mt-12 gap-y-4 px-2'>
-        <button className='bg-red-500 rounded-md py-2'>Product</button>
-        <button className='bg-red-500 rounded-md py-2'>Pengguna</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Keranjang</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Tabel</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Pemasaran</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Penjadwalan</button>
       </div>
     </div>
+    // content start
+    // content 
   );
+  
 };
 
+
 export default Sidebar;
+
+
+
