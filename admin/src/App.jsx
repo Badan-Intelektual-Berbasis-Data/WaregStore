f4f80c47d186cda4ea8f1746e8d8692f31856e0a
uqizeroxl <mohkholqimaulana@gmail.com>
2024-07-01 15:00:22 +0700

    membuat sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [active, setActive] = useState(false)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>hallo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex'>
+    <Sidebar opened={active} />
+    <div>
+      <button onClick={() => setActive(state => !state)}>
+        Open
+      </button>
+    </div>
+    </div>
   )
 }
 
