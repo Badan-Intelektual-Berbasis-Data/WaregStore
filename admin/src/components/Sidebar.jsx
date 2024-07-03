800a65d02746219bee2a05d06939931b9c2b8dbc
uqizeroxl <mohkholqimaulana@gmail.com>
2024-07-03 10:02:14 +0700

    ngubah style

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Sidebar = ({opened}) => {
   return (
-    <div className={`max-h-screen bg-[#808080] py-8 text-center overflow-hidden duration-150 ease-in ${!opened ? 'w-0' : 'w-[250px]'}`}>
+    <div className={`min-h-screen bg-[#808080] py-8 text-center overflow-hidden duration-150 ease-in ${!opened ? 'w-0' : 'w-[250px]'}`}>
       <h1 className='font-bold text-2xl text-white'>WaregStore</h1>
       <div className='flex flex-col mt-12 gap-y-4 px-2'>
         <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Keranjang</button>
