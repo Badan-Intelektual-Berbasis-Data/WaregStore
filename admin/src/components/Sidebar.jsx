a48da7632bc4a1a4552886417fa4f7066fb96fab
khoirurrohman <rohmanjr027@gmail.com>
2024-07-03 21:13:28 +0700

    merubah nama di sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,10 +6,12 @@ const Sidebar = ({opened}) => {
     <div className={`min-h-screen bg-[#808080] py-8 text-center overflow-hidden duration-150 ease-in ${!opened ? 'w-0' : 'w-[250px]'}`}>
       <h1 className='font-bold text-2xl text-white'>WaregStore</h1>
       <div className='flex flex-col mt-12 gap-y-4 px-2'>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Keranjang</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Tabel</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Pemasaran</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Penjadwalan</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>dashboard</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>arders</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Product</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>customers</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>buyer</button>
+        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>account</button>
       </div>
     </div>
     // content start
