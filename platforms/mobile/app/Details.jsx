8a1df72fcb9e5c758fc1d9a87bacf8563afca84f
Yogaseptiandwipratama <yogaseptianpratama75@gmail.com>
2024-07-03 13:23:53 +0700

    tessaja

--- platforms/mobile/app/Details.jsx
+++ platforms/mobile/app/Details.jsx
@@ -1,5 +1,5 @@
 import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React from "creact";
 import AmmountButton from "@/components/AmmountButton";
 
 // halo
