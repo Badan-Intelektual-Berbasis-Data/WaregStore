98878d5260d7d7deac1427f6033381d41dfdb0f3
khoirurrohman <rohmanjr027@gmail.com>
2024-07-04 13:42:25 +0700

    menghapus isian table product

--- admin/src/pages/Product.jsx
+++ admin/src/pages/Product.jsx
@@ -7,23 +7,7 @@ function Product(){
        <div>
          <h1 className="blue-line">PRODUCT</h1>
          <table>
-           <thead>
-             <tr>
-               <th>ID</th>
-             </tr>
-             <tr>
-               <th>GAME</th>
-             </tr>
-             <tr>
-               <th>ENTITY</th>
-             </tr>
-             <tr>
-               <th>STOCK</th>
-             </tr>
-             <tr>
-               <th>STATUS</th>
-             </tr>
-           </thead>
+           
           
          </table>
        </div>
