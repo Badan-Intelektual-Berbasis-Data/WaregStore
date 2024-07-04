49cb1a56729667ecbd00d8302027d40d66dd07eb
khoirurrohman <rohmanjr027@gmail.com>
2024-07-04 12:07:51 +0700

    coba coba

--- admin/src/components/product.jsx
+++ admin/src/components/product.jsx
@@ -1,25 +1,23 @@
-import Product from './components/Product'
-function Product(){
-    return(
+// src/components/Sidebar.js
+import React from 'react';
 
-       <div>
-        <h1>hallo</h1>
-        <div className={`min-h-screen bg-[#808080] py-8 text-center overflow-hidden duration-150 ease-in ${!opened ? 'w-0' : 'w-[250px]'}`}>
-      <h1 className='font-bold text-2xl text-white'>WaregStore</h1>
-      <div className='flex flex-col mt-12 gap-y-4 px-2'>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>dashboard</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>arders</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>Product</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>customers</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>buyer</button>
-        <button className='bg-[#D9D9D9] rounded-md py-2 font-medium'>account</button>
+const Product = ({opened}) => {
+  return (
+    <div>
+      <div>
+        <h1>hello word</h1>
       </div>
     </div>
-    <diV>
         
-    </diV>
-       </div> 
-    )
-} 
+    
+    // content start
+    // content 
+  );
+  
+};
 
-export default Product 
+
+export default Product;
+
+
+
