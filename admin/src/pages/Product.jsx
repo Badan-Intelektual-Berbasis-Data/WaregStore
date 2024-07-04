c674c0ade01b79a7bffafb1f6f1a577ad927169a
khoirurrohman <rohmanjr027@gmail.com>
2024-07-04 13:39:15 +0700

    buat claas produc dan mengaturnya di app.css

--- admin/src/pages/Product.jsx
+++ admin/src/pages/Product.jsx
@@ -1,10 +1,40 @@
 function Product(){
-    return(
+ 
 
+   
+   
+     return (
        <div>
-          <h1>Hello worlds</h1>
-       </div> 
-    )
-} 
+         <h1 className="blue-line">PRODUCT</h1>
+         <table>
+           <thead>
+             <tr>
+               <th>ID</th>
+             </tr>
+             <tr>
+               <th>GAME</th>
+             </tr>
+             <tr>
+               <th>ENTITY</th>
+             </tr>
+             <tr>
+               <th>STOCK</th>
+             </tr>
+             <tr>
+               <th>STATUS</th>
+             </tr>
+           </thead>
+          
+         </table>
+       </div>
+     );
+   };
+   
+   
+   
+   
+   
+
 
 export default Product 
+
