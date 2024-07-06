8e164e8ce814e35eb51f65f021f23724fc5818b2
uqizeroxl <mohkholqimaulana@gmail.com>
2024-07-06 10:40:57 +0700

    menambahkan dashboard

--- admin/src/pages/Layout.jsx
+++ admin/src/pages/Layout.jsx
@@ -8,7 +8,7 @@ function Layout() {
   return (
     <div className='flex'>
         <Sidebar opened={active}/>
-        <div>
+        <div className='w-full'>
             <button onClick={() => setActive(state => !state)}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
