1323d8cfb56a21ce39df7b36d3a50b6a023e34f2
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-07-06 07:50:50 +0700

    Update CORS origin

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -32,12 +32,13 @@ SECRET_KEY = 'django-insecure-1ldgzft6by6j*av$-58rtg*i)2x*m83@9)j_t8^s_n2#ydi*+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:3000', 'api.waregstore.biz.id', 'localhost:8081']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:3000', 'api.waregstore.biz.id', 'localhost:8081', '127.0.0.1:8081']
 
 CORS_ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '127.0.0.1:3000',
+    '127.0.0.1:8081',
     'api.waregstore.biz.id',
     'localhost:8081'
 ]
