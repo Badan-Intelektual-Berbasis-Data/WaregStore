6cc5f37450298fd35e68b935b7f955a9c8127b6b
wahyudiRomadhoni <wahyudiromadoni69@gmail.com>
2024-07-06 11:26:47 +0700

    perubahan di page pembayaran

--- platforms/mobile/app/Pembayaran.jsx
+++ platforms/mobile/app/Pembayaran.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { RadioButton } from "react-native-paper";
 
 export default function PembayaranScreen() {
-  const [paymentMethod, setPaymentMethod] = useState('');
-  const [bank, setBank] = useState('');
-  const [eWallet, setEWallet] = useState('');
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const [bank, setBank] = useState("");
+  const [eWallet, setEWallet] = useState("");
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -22,51 +30,75 @@ export default function PembayaranScreen() {
 
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Nomor WhatsApp:</Text>
-        <TextInput style={styles.input} placeholder=" " keyboardType="phone-pad" />
-        <Text style={styles.noteText}>* Akan dihubungi apabila terjadi masalah</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=" "
+          keyboardType="phone-pad"
+        />
+        <Text style={styles.noteText}>
+          * Akan dihubungi apabila terjadi masalah
+        </Text>
       </View>
 
       <Text style={styles.paymentOptionsTitle}>Pembayaran</Text>
       <View style={styles.paymentOptionsContainer}>
-        <TouchableOpacity
-          style={[styles.paymentOption, paymentMethod === 'bank' && styles.selectedOption]}
-          onPress={() => setPaymentMethod('bank')}
-        >
-          <Text style={styles.paymentOptionText}>Transfer Bank</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.paymentOption, paymentMethod === 'ewallet' && styles.selectedOption]}
-          onPress={() => setPaymentMethod('ewallet')}
-        >
+        <View style={[styles.paymentOption, paymentMethod === "ewallet"]}>
+          <RadioButton
+            value="ewallet"
+            status={paymentMethod === "ewallet" ? "checked" : "unchecked"}
+            onPress={() => setPaymentMethod("ewallet")}
+            color={paymentMethod === "ewallet" ? "pink" : "black"}
+          />
           <Text style={styles.paymentOptionText}>E-Wallet</Text>
-        </TouchableOpacity>
+        </View>
+        <View style={[styles.paymentOption, paymentMethod === "bank"]}>
+          <RadioButton
+            value="second"
+            status={paymentMethod === "bank" ? "checked" : "unchecked"}
+            onPress={() => setPaymentMethod("bank")}
+            color={paymentMethod === "bank" ? "pink" : "black"}
+          />
+          <Text style={styles.paymentOptionText}>Transfer Bank</Text>
+        </View>
       </View>
 
-      {paymentMethod === 'bank' && (
+      {paymentMethod === "bank" && (
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Pilih Bank</Text>
           <View style={styles.bankOptionsContainer}>
             <TouchableOpacity
-              style={[styles.bankOption, bank === 'bca' && styles.selectedOption]}
-              onPress={() => setBank('bca')}
+              style={[
+                styles.bankOption,
+                bank === "bca" && styles.selectedOption,
+              ]}
+              onPress={() => setBank("bca")}
             >
               <Text style={styles.paymentOptionText}>Bank BCA</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={[styles.bankOption, bank === 'bni' && styles.selectedOption]}
-              onPress={() => setBank('bni')}
+              style={[
+                styles.bankOption,
+                bank === "bni" && styles.selectedOption,
+              ]}
+              onPress={() => setBank("bni")}
             >
               <Text style={styles.paymentOptionText}>Bank BNI</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={[styles.bankOption, bank === 'mandiri' && styles.selectedOption]}
-              onPress={() => setBank('mandiri')}
+              style={[
+                styles.bankOption,
+                bank === "mandiri" && styles.selectedOption,
+              ]}
+              onPress={() => setBank("mandiri")}
             >
               <Text style={styles.paymentOptionText}>Bank Mandiri</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={[styles.bankOption, bank === 'bri' && styles.selectedOption]}
-              onPress={() => setBank('bri')}
+              style={[
+                styles.bankOption,
+                bank === "bri" && styles.selectedOption,
+              ]}
+              onPress={() => setBank("bri")}
             >
               <Text style={styles.paymentOptionText}>Bank BRI</Text>
             </TouchableOpacity>
@@ -74,31 +106,43 @@ export default function PembayaranScreen() {
         </View>
       )}
 
-      {paymentMethod === 'ewallet' && (
+      {paymentMethod === "ewallet" && (
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Pilih E-Wallet</Text>
           <View style={styles.bankOptionsContainer}>
             <TouchableOpacity
-              style={[styles.bankOption, eWallet === 'ovo' && styles.selectedOption]}
-              onPress={() => setEWallet('ovo')}
+              style={[
+                styles.bankOption,
+                eWallet === "ovo" && styles.selectedOption,
+              ]}
+              onPress={() => setEWallet("ovo")}
             >
               <Text style={styles.paymentOptionText}>OVO</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={[styles.bankOption, eWallet === 'gopay' && styles.selectedOption]}
-              onPress={() => setEWallet('gopay')}
+              style={[
+                styles.bankOption,
+                eWallet === "gopay" && styles.selectedOption,
+              ]}
+              onPress={() => setEWallet("gopay")}
             >
               <Text style={styles.paymentOptionText}>GoPay</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={[styles.bankOption, eWallet === 'dana' && styles.selectedOption]}
-              onPress={() => setEWallet('dana')}
+              style={[
+                styles.bankOption,
+                eWallet === "dana" && styles.selectedOption,
+              ]}
+              onPress={() => setEWallet("dana")}
             >
               <Text style={styles.paymentOptionText}>Dana</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              style={[styles.bankOption, eWallet === 'shopeepay' && styles.selectedOption]}
-              onPress={() => setEWallet('shopeepay')}
+              style={[
+                styles.bankOption,
+                eWallet === "shopeepay" && styles.selectedOption,
+              ]}
+              onPress={() => setEWallet("shopeepay")}
             >
               <Text style={styles.paymentOptionText}>ShopeePay</Text>
             </TouchableOpacity>
@@ -117,18 +161,18 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
-    backgroundColor: '#f9f9f9',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#f9f9f9",
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   inputContainer: {
-    width: '100%',
+    width: "100%",
     marginBottom: 15,
   },
   label: {
@@ -137,66 +181,66 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 20,
     paddingLeft: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   noteText: {
     fontSize: 12,
-    color: 'red',
+    color: "red",
     marginTop: 5,
   },
   paymentOptionsTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 20,
     marginBottom: 10,
   },
   paymentOptionsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
     marginBottom: 20,
   },
   paymentOption: {
-    backgroundColor: '#ddd',
+    flexDirection: "row",
     padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    width: '45%',
+    borderRadius: 20,
+    alignItems: "center",
+    width: "45%",
   },
   selectedOption: {
-    backgroundColor: '#f7287b',
+    backgroundColor: "#f7287b",
   },
   paymentOptionText: {
-    fontSize: 16,
-    color: 'black',
+    fontSize: 14,
+    color: "black",
   },
   bankOptionsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
   bankOption: {
-    backgroundColor: '#ddd',
+    backgroundColor: "#ddd",
     padding: 10,
     borderRadius: 10,
-    alignItems: 'center',
-    width: '45%',
+    alignItems: "center",
+    width: "45%",
     marginVertical: 5,
   },
   orderButton: {
     marginTop: 30,
-    backgroundColor: '#0099FF',
+    backgroundColor: "#0099FF",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
-    width: '100%',
+    alignItems: "center",
+    width: "100%",
   },
   orderButtonText: {
     fontSize: 18,
-    color: '#fff',
+    color: "#fff",
   },
 });
