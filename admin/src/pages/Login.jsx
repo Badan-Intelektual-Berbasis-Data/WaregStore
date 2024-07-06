e525d0c1692e39261d8555952e0f5db8c4fd9338
MuhammadRidwanollah <caca.mokong@gmail.com>
2024-07-06 11:33:26 +0700

    menambahkan login

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,13 +1,21 @@
 
-export default function Login() {
-    return(
-    <div className="flex items-center justify-center min-h-screen">
-        <div className="items-center bg-white border-8 border-gray-300 shadow-lg p-6 bg-white rounded-lg">
-            <h1 className="">Login</h1>
-            <input type="text" />
-        </div>
-    </div>
-    );
-};
+function Login() {
+    return (
+          <div className="w-full h-full flex justify-center items-center bg-red-500">
+              <div className="bg-red-500 w-max p-4">
+                  <h1>Login</h1>
+                  <div>
+                      <p>Username</p>
+                      <input type="text" />
+                  </div>
+                  <div>
+                      <p>Password</p>
+                      <input type="text" />
+                  </div>
+              </div>
+          </div>
+    )
+  }
   
-
+  export default Login
+  
